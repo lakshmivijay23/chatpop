eb8e1ffa25a7d5826ee2fa9b4cefc2761fc518ac
Lakshmi Vijayan <146443780+lakshmivijay23@users.noreply.github.com>
2025-12-26 23:22:20 +0530

    Remove secret key from settings.py
    Removed the secret key for security purposes.

--- chatbot_demo/settings.py
+++ chatbot_demo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vs)01=9#g$w3)skkf@gth2s-jlvjo40u(2=wi8480kts#si&us'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
